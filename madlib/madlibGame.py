43458983d59fe5b6b375daf5336a5e803b163ea1
CrazyJ36 <crazy_j36@hotmail.com>
2018-03-13 11:06:49 +0000

    improve user experience: make story sound better in most cases.

--- madlib/madlibGame.py
+++ madlib/madlibGame.py
@@ -2,4 +2,4 @@ wd1 = raw_input("Type A Verb word: ")
 wd2 = raw_input("Type A Noun: ")
 wd3 = raw_input("Think crazy: ")
 
-print "I'm",wd1, "your",wd2,"...",wd3
+print "I",wd1, "your",wd2,"...",wd3
