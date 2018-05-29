1e347f11017acbd8a6fee03faeed20b8085e46e2
CrazyJ36 <crazy_j36@hotmail.com>
2018-05-29 07:48:20 -0400

    u

--- importFun/service.py
+++
@@ -1,6 +0,0 @@
-import test1
-def serviceFunc():
- if __name__ == '__main__':
-  test1.test1Func()
-  test1.someFunc()
-serviceFunc()
--- importFun/test1.py
+++
@@ -1,8 +0,0 @@
-def someFunc():
- print("other function from test1, someFunc")
-
-def test1Func():
- print("test1 Function. Printing from test1")
- if __name__ == '__main__':
-  test1Func()
-  someFunc()
