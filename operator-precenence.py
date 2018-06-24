15960892f730856bf7a094ead0b6e2ad3f221edc
CrazyJ36 <crazy_j36@hotmail.com>
2018-06-24 15:53:50 -0400

    all operators

--- operator-precenence.py
+++ operator-precenence.py
@@ -7,10 +7,19 @@
 # List of operators from highest
 # precenece to lowest:
 #
-# **    Exponentiation (raise to the power)
-# ~+-   Complement, unary plus minus (method names +@ -@)
-# */%//   Multiply, division, modulo, floor division
-# +-    Addition, subtraction
+# **    -Exponentiation (raise to the power)
+# ~+-   -Complement, unary plus minus (method names +@ -@)
+# * / % //   -Multiply, division, modulo, floor division
+# +-    -Addition, subtraction
+# >> <<  -right and left bitwise shift
+# &      -Bitwise 'AND'
+# ^|    -Bitwise exclusive `OR' and regulat `OR'
+# <= < > >=   -Comparison operators
+# <> == !=   -Equality operators
+# = %= /= //= -= +- *= **=   -Assignment operators
+# is, is not   -identity operators
+# in, not in   -Membership operators
+# not, and, or   -Logical operators
 # Research these more
 
 x = 1 + (2 * 2)
