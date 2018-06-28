bd01184a21b7dbf53410a998800b01ff0edcf7b8
CrazyJ36 <crazy_j36@hotmail.com>
2018-06-28 18:43:40 -0400

    u

--- curses-app.py
+++ curses-app.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 import curses
-window = curses.initscr() # start an ncurses screen
 
+window = curses.initscr()
 curses.noecho()
 curses.cbreak()
 window.keypad(True)
-
-window.border(0)  # makes a visible border like a window on screen
-window.addstr(1, 1, "Hello!")
+window.border(3)
+help = "q: quit\n d: Show developer name"
+window.addstr(1, 1, help)
 window.refresh()
+
+win1 = curses.newwin(5, 40, 7, 20)
+win1.border(0)
+win1.addstr(1, 1, "CrazyJ36")
 
 loop = True
 while loop:
@@ -16,3 +20,5 @@ while loop:
   if choice == 113:
     loop = False
     curses.endwin()
+  elif choice == 100:
+    win1.refresh()
