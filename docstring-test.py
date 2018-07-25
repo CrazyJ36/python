c88dc42ac5736f5377b4bf71172bc5da93723612
CrazyJ36 <crazy_j36@hotmail.com>
2018-07-25 15:30:12 -0400

    A comment definition

---
+++ docstring-test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+print("A print() statement")
+
+def myPrint():
+  """
+   -----
+   A Docstring;
+   Should document what
+   the function or module does.
+   This text doesn't show
+   during execution.
+   -----
+   This myPrint function
+   simply prints A string.
+  """
+  print("function done")
+
+myPrint()
+print("app done") # A source code comments.
+                  # Should document how
+                  # a statement works. 
