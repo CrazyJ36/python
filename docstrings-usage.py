05ae5d5278e7c4d1f0d177076f67850abae34e37
CrazyJ36 <crazy_j36@hotmail.com>
2018-07-25 16:54:31 -0400

    how to document my functions

---
+++ docstrings-usage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+print("code started.")
+
+def myDocPrintFunc():
+  """
+   -----
+   A Docstring;
+   Should document what
+   the function or module does.
+   This text, by default,
+   doesn't show during program execution,
+   and can be read by developers.
+   Though you can use:\n
+   print(mfunc.__doc__)
+   Anytime in the code
+   to print this as A help function.\n
+   To show an interactive help
+   (like manpage) use:
+   help(mfunc) or
+   print(help(mfunc))
+   -----
+   This myPrint function
+   simply prints A string.
+  """
+  print("function done")
+
+task = input("""Use:
+i - for interactive help
+h - for inline help
+""")
+if task == "i":
+  help(myDocPrintFunc)
+elif task == "h":
+  print(myDocPrintFunc.__doc__)
+
+myDocPrintFunc()
+print("app done")
+
+# As opposed to docstring,
+# A source code comment (# comment)
+# Should document how
+# a statement works.
---
+++ function-reuse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# the parameters(x,y) of my add can be set directly
+# set when calling the function.
+def myAdd(x, y):
+  return x + y
+
+y = 2
+# assigning function to varaible
+name = myAdd
+print(name(3, y))
+
+# functions can be arguments of other functions
+def myPrint():
+  print(myAdd(2, 2))
+
+myPrint()
