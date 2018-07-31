0328cbf0f9a629e7579ff3083237a37fb7b4c808
CrazyJ36 <crazy_j36@hotmail.com>
2018-07-30 21:22:03 -0400

    write file/ notes app

---
+++ notes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# This creates/appends personal notes,
+# one at A time, run after run, to A
+# notes.txt file at home directory.
+
+op = open("../../notes.txt", 'a')
+text = input("New note:\n")
+op.write(text)
+op.write("\n")
+op.close()
+
