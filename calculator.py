8e8e68b9b0d7f430fc16bcc4b1f0534c282910b0
CrazyJ36 <crazy_j36@hotmail.com>
2018-08-10 14:30:49 -0400

    fix

--- calculator.py
+++ calculator.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
 
-import re
-
 def add(x, y):
     return x + y
 def sub(x, y):
     return x - y
+    
 
 try:
     num1 = int(input("number1:    "))
 except ValueError:
-	print("numbers must use 0-9's")
-	exit()
-	
+    print("numbers must use 0-9's")
+    exit()
+    
 sym = input("+, -:       ")
+if sym != '+' and sym != '-':
+    print("use only '+' or '-' character as equator")
+    exit()
 
 try:
     num2 = int(input("number2:    "))
 except ValueError:
-	print("numbers must use 0-9's")
-	exit()
-
-if sym != '+' and sym != '-':
-    print("use only '+' or '-' character as equator")
+    print("numbers must use 0-9's")
     exit()
-elif sym == "+":
+
+if sym == "+":
     print("answer:    ",add(num1,num2))
 elif sym == "-":
     print("answer:    ",sub(num1,num2))
