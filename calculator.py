b20b3503086f3bdfaf658430276fb45df1fe4aee
CrazyJ36 <crazy_j36@hotmail.com>
2018-08-10 14:06:50 -0400

    calculator: a fix for invalid number input

--- calculator.py
+++ calculator.py
@@ -1,27 +1,30 @@
-<<<<<<< HEAD
 #!/usr/bin/env python3
-=======
-#!/usr/bin/env python3.6
+
 import re
->>>>>>> 6d52c1eb1254f71e0b979d8b6fafe138e645afd2
 
 def add(x, y):
     return x + y
 def sub(x, y):
     return x - y
 
-num1 = int(input("number1:    "))
+try:
+    num1 = int(input("number1:    "))
+except ValueError:
+	print("numbers must use 0-9's")
+	exit()
+	
 sym = input("+, -:       ")
-num2 = int(input("number2:    "))
 
-<<<<<<< HEAD
-if sym == "+":
-=======
+try:
+    num2 = int(input("number2:    "))
+except ValueError:
+	print("numbers must use 0-9's")
+	exit()
+
 if sym != '+' and sym != '-':
     print("use only '+' or '-' character as equator")
     exit()
 elif sym == "+":
->>>>>>> 6d52c1eb1254f71e0b979d8b6fafe138e645afd2
     print("answer:    ",add(num1,num2))
 elif sym == "-":
     print("answer:    ",sub(num1,num2))
