93edc21f3ba27a4f8a42c4908dc5393fff556ebe
CrazyJ36 <crazy_j36@hotmail.com>
2018-08-10 15:27:47 -0400

    slicing and power of(*) math

---
+++ list-comprehension.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# A quick list with to power of amount rule in range
+
+nums = [i*2 for i in range(10)]
+print(nums)
--- slicing-range-lists-dict-tuple.py
+++ slicing-range-lists-dict-tuple.py
@@ -1,6 +1,30 @@
 #!/usr/bin/env python3
 
-# reuse a list, 'sliced' as: [start:end] 
+# reuse a list, 'sliced' as: [start:end]
 
-mlist = ["bill", "james", "becky", "chris"]
-print(mlist[0:2]) # print mlist items at index: from 0 to 2, not including 2
+mlist = ["bill", "james", "becky", "chris", "thomas", "megan"]
+
+print("my list, defined like ['','','']:")
+print(mlist)
+
+print("[0:2] slicing from index 0 to 2, not including 2:")
+print(mlist[0:2])
+
+# if start: or :end part is left out, start or end
+# of list,tuple is taken.
+
+print("[:2] from start to before second index of mlist:")
+print(mlist[:2])
+print("[2:] from 2nd to last index:")
+print(mlist[2:])
+
+print("[::] all:")
+# like above, [::] means slice from start to end
+print(mlist[::]) # will print all
+
+# steps between indexes can be indicated
+print("[::2] start to end. every steps/intervals of 2:")
+print(mlist[::2])
+
+# can use negative index positions to count backwards
+print("[-1] backwards 1 index:", mlist[-1])
