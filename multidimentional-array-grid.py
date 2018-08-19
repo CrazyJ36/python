8b27c4426f8883155c41854b79b19f0d8cbb9db4
CrazyJ36 <crazy_j36@hotmail.com>
2018-08-19 15:50:05 -0400

    u

---
+++ multidimentional-array-grid.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# The 'grid' to be worked with.
+'''
+   1 2 3
+   2 4 5
+   3 5 6
+'''
+
+my_array = [[1,2,3],[3,2,1]]
+print(my_array[0][1])  # first array, second item
+print(my_array[1][1])  # second array, second item
--- musical-modes.py
+++ musical-modes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from collections import deque
 
 I = "1. Ionian"
