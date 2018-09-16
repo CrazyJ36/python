fe286066f731e6b2ab2749504eb71f76c453b7d5
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-16 12:20:00 -0400

    basic git status

--- tk-gitStatus.py
+++ tk-gitStatus.py
@@ -4,37 +4,31 @@ from tkinter import *
 
 win = Tk()
 win.pack_propagate(0)
-win.geometry("600x500")
-win.title(string="App")
-repo = Entry(win, text="repo name")
+win.title(string="Git Status")
+win.geometry("400x300")
+repo = Entry(win)
 
-
-if sys.platform == "win32" and os.scandir("C:\\Users\\Thomas"):
+if sys.platform == "win32" and os.environ['USERNAME'] == "Thomas":
     devdir = "C:\\Users\\Thomas\\Development\\"""
-elif sys.platform == "win32" and os.scandir("C:\\Users\\CrazyJ36"):
+elif sys.platform == "win32" and os.environ['USERNAME'] == "CrazyJ36":
     devdir = "C:\\Users\\CrazyJ36\\Development\\"""
 elif sys.platform == "linux":
     devdir = "/home/thomas/development/"
 else:
     envwarn = Label(win, text="failed to get environment")
     envwarn.pack()
-    exit(0)
 
 
 def gitcmd():
     cmdstr = "cd " + devdir + repo.get() + " && git status"
-    cmdlbl = Label(win, text="Using: " + cmdstr)
-    cmdlbl.pack()
-    outfileobj = os.popen(cmdstr)
-    out = outfileobj.read()
-    outfileobj.close()
+    cmdfileobj = os.popen(cmdstr)
+    out = cmdfileobj.read()
+    cmdfileobj.close()
     outtxt = Label(win, text=out)
     outtxt.pack()
 
-if win.keys(win, ENTER)
+
 btn = Button(win, text="Check Git Repo", command=lambda: gitcmd())
 repo.pack()
 btn.pack()
-
 win.mainloop()
-
