cfa43836a9b789057cf7d0645bad9e5f2543c2f6
CrazyJ36 <crazy_j36@hotmail.com>
2018-09-16 08:15:20 -0400

    check-dir for different pc usernames

---
+++ check-dir.py
@@ -0,0 +1,8 @@
+import os
+
+if os.scandir("C:\\Users\\Thomas"):
+    print("Thomas User(Tohsiba Laptop User)")
+elif os.scandir("C:\\Users\\CrazyJ36"):
+    print("CrazyJ36 User(Win Azure PC)")
+elif os.scandir("/home/thomas/"):
+    print("Linux user")
