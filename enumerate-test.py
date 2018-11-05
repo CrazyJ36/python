72bbd925cb21183068080df69976104050e44cde
CrazyJ36 <crazy_j36@hotmail.com>
2018-11-05 01:32:15 -0500

    enums.py: basic enums list

--- enumerate-test.py
+++ enumerate-test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-# enumerate() iterates through values and indices of a list.
+
+# enumerate() makes values an enum.
+# iterates through values and indices of a list.
 
 mlist = (1,2,3,4,5)
 for i in enumerate(mlist):
---
+++ enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+class tst(Enum):
+	ONE=1
+	TWO=2
+	THREE=3
+
+print(tst.ONE.name)
