795f59ef4110481ed901cc486bd454344a782c28
CrazyJ36 <crazy_j36@hotmail.com>
2019-05-10 22:46:41 -0400

    u

--- pi/rpi-gpio/flash-four.py
+++ pi/rpi-gpio/flash-four.py
@@ -15,21 +15,23 @@ for n in l:
 # Turn on all
 for n in l:
   GPIO.output(n, GPIO.HIGH)
-sleep(5)
+sleep(1)
 # Turn off all
 for n in l:
   GPIO.output(n, GPIO.LOW)
 
+print "push buttons to test.."
 # BUTTONS
 b = [14, 26]
 for x in b:
-  GPIO.setup(x, GPIO.IN)
+  GPIO.setup(x, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-while GPIO.input( b[0] ) == GPIO.LOW:
+while GPIO.input( b[1] ) == GPIO.LOW:
   sleep(0.1)
-  if GPIO.input( b[0] ) == GPIO.HIGH:
-    print "pushed button on gpio 14"
+  if GPIO.input( b[1] ) == GPIO.HIGH:
+    print "Button 14 pushed.."
 
+print "Exiting.."
 GPIO.cleanup()
 exit()
 
