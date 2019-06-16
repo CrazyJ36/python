b40840e604321161d86094cadb992652b33e149a
CrazyJ36 <crazy_j36@hotmail.com>
2019-06-16 20:30:42 +0000

    explain short-circuit calculation

--- short-circuit-precenence.py
+++ short-circuit-precenence.py
@@ -19,7 +19,14 @@ else:
 # second operator in if/and would cancel out and return false
 # if the firsr operator is false. This even overrides precedence
 # if the second operator were in perenthesis.
+# Obviously, even though true is true, the first comparator is false
 if 3 < 2 and (True == True):
   print("True in:\nif 3<2 and (True == True)")
 else:
   print("False in:\nif 3<2 and (True == True)")
+
+# TODO: this would be best explained when calculating voltages
+# and current in A program.
+# Operations in perenthesis
+# should first come first(current), and, if voltage is the same,
+# after drawing current, continue.
