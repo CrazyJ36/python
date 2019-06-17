93de6bed369baa51c40ad0f8f3a84185e50bda9c
CrazyJ36 <crazy_j36@hotmail.com>
2019-06-17 00:34:08 +0000

    test in keyword as boolean

---
+++ boolean-in.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Like with for loops, the 'in' keyword itslef can be used
+# as A boolean operator.
+
+word = 'five'
+print("word is 'five'\n")
+
+if 'i' in word:
+  print("there's an 'I' in word")
+
+if not 'x' in word:
+  print("there is no 'x' in word")
+
+print("\n'f' in word?:")
+print( 'f' in word )
+
