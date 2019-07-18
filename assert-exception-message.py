7b00ab36b1aaa29cf4fe9364c2f587118c3e4572
CrazyJ36 <crazy_j36@hotmail.com>
2019-07-18 02:11:39 -0400

    assert-exception-massage: makes more sense as for what assert does

--- assert-exception-message.py
+++ assert-exception-message.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 
-# purposeful false assertio.
+# This shows how to show A custom
+# error message when you 'assert' A
+# statement to return true, but turns out false.
+
+print("""Trying A 'statement'. If error occurs
+in the statememt, A known-error message will
+be shown instead of an AssertionError.\n""")
 try:
-	assert False, "My failed assertion message."
-except AssertionError as e:
-	print(e.args)
+    assert False, "Can not assert False statement."
+    print("Asserted True.")
+except AssertionError as assertError1Var:
+    print(assertError1Var.args)
+
+print("\nDone. Exiting...")
