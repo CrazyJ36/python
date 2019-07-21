accaaf1fbc79c9a0ea60282b1f0c3c1efa18683f
CrazyJ36 <crazy_j36@hotmail.com>
2019-07-21 01:49:57 -0400

    regex-print-specific-lines-from-http-url: fix

---
+++ regex-print-specific-lines-from-http-url.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+import urllib.request
+import re
+
+handler = urllib.request.urlopen('https://www.google.com/robots.txt')
+
+for line in handler:
+    if re.search( 'Allow', line.decode().strip() ):
+        print(line)
+    
+
+
+
