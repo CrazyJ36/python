5c6f2b425d6f4083113bf2afe56fc896d56a0da9
CrazyJ36 <crazy_j36@hotmail.com>
2019-07-21 02:05:10 -0400

    u

--- regex-print-specific-lines-from-http-url.py
+++ regex-print-specific-lines-from-http-url.py
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env python3
 import urllib.request
 import re
 
-handler = urllib.request.urlopen('https://www.google.com/robots.txt')
+handler = urllib.request.urlopen(
+    'https://www.google.com/robots.txt')
 
 for line in handler:
     if re.search( 'Allow', line.decode().strip() ):
         print(line)
-    
+
 
 
 
