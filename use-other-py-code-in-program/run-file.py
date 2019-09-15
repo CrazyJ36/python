75592327445b1e370c55bf579eb246ecd42b730e
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-15 19:06:40 -0400

    work on py files as headers to import

--- use-other-py-code-in-program/run-file.py
+++ use-other-py-code-in-program/run-file.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 print("This is run-file.py\n")
 import printString
+
+# This runs the only statement in other file
+# because thats what the other file does, there
+# are no functions to run from other file
+
+#TODO: Make printString A function in other file.
+# import 'the file', then run printString function.
+# (in A different project)
