b221eaf53f7cf1191e94d4e3ccff5776586b954f
CrazyJ36 <crazy_j36@hotmail.com>
2019-09-16 20:12:27 -0400

    pi: always running default program shutsdown system on button click

--- pi/gpiozero/default_program.py
+++ pi/gpiozero/default_program.py
@@ -6,13 +6,22 @@
 
 from gpiozero import LED, Button
 from time import sleep
+from subprocess import call
 
 led = LED(6)
 btn = Button(8)
 
-while True:
+def flash():
   led.on()
-  sleep(1)
+  sleep(0.1)
   led.off()
-  sleep(1)
-  if btn.is_pressed: exit()
+  sleep(0.1)
+
+
+while True:
+  if btn.is_pressed:
+    flash()
+    flash()
+    flash()
+    call(['sudo', 'poweroff'])
+    exit()
