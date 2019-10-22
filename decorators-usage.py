76c810cb15f4e3152ac37460dce566664a4fef83
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-22 00:10:10 -0400

    decorators n stuff

---
+++ decorators-usage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# decorators allow to reuse A function in A new way to extend it's function.
+
+def decor(func):
+  def wrap():
+    print("-----")
+    func()
+    print("-----")
+
+  return wrap
+
+def print_text1():
+  print("Hello")
+
+def print_text2():
+  print("World")
+
+decor1 = decor(print_text1)
+decor2 = decor(print_text2)
+
+decor1()
+decor2()
+print()  # skip line
+
+# The same, simpler
+def title(text):
+  print("-----")
+  print(text)
+  print("-----")
+
+title("Hello")
+title("World")
--- function-define.py
+++ function-define.py
@@ -3,3 +3,6 @@
 def line():
     print("-"*10)
 line()
+
+# you can put def in def to 'wrap' function or use decorators.
+
