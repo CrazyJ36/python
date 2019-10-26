6ac00ec2eb8cfef5e5acb5d850111ce0bcd1899f
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-26 01:55:58 -0400

    start renaming files to use underscores

--- var_names_syntax.py
+++ var_names_syntax.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+# Python coding styles highly recommend underscores
+# instead of dashes. For module names as well.
+
 # Testing A variable which points to number with
 # A leading Zero
+
 # It won't work
 my_num = 08
 
