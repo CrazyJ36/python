8141fd06d577c40ed06470c2aa26724f0950f89b
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-26 07:45:06 -0400

    pysimplegui: event and value tests.

---
+++ pysimplegui/text-event.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+
+layout = [
+  # can't get clicks(if event == 'text_id'
+  # on text without enable_events, therefor not values either.
+  # also don't use click_submits, deprecated.
+  [sg.Text('click me', key='text_id', enable_events=True)]
+]
+
+window = sg.Window('Title', layout)
+
+while True:
+  event, values = window.Read()
+
+  if event == 'text_id':
+    print('\ntext clicked!\n' + 'event=', event) # 'con' + 'cat', no space.
+    # trying to get values from text_id raised keyerror
+  if event is None:
+    print('close clicked')
+    break
+
+print('done.')
+window.Close()
+exit(0)
