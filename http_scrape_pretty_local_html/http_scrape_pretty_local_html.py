8b0a50bcb5b945cb3aa8519897da45536d264367
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-27 18:43:37 -0400

    more http

--- http_scrape_pretty_local_html/http_scrape_pretty_local_html.py
+++ http_scrape_pretty_local_html/http_scrape_pretty_local_html.py
@@ -10,11 +10,12 @@ if os.name == 'nt':
     file_url = 'file:///C:/Users/' + user + \
         '/development/python/http_scrape_pretty_local_html/index.html'
 
-elif os.name == 'linux':
+elif os.name == 'posix':
     file_url = 'file:///home/' + user + \
         '/development/python/http_scrape_pretty_local_html/index.html'
 else:
-    file_url = 'file:///data/data/com.termux/files/home/development/python/http_scrape_pretty_local_html/index.html'
+    print('unknown os, set os.name in script')
+    exit()
 
 with urllib.request.urlopen(file_url) as response:
     html = str(response.read())
@@ -32,7 +33,7 @@ with urllib.request.urlopen(file_url) as response:
     #  Above .replace works for the string statement before.
     # Replaced the 'apostraphe' escape code with the actual char.
 
-    html_body_text = html_body.split('\\r' + '\\n')  # is list
+    html_body_text = html_body.split('\\n' + '\\r')  # is list
 
     # putting list items in string.
     string = ''
@@ -42,5 +43,5 @@ with urllib.request.urlopen(file_url) as response:
 # lstrip() strips whitespace on the left (as opposed to rstring() - right)
 # This will make the terminal print text pretty regardless
 #  of where spaces are on new lines.
-print(string.lstrip())
+print(string.lstrip().rstrip())
 exit(0)
