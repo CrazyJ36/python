27e49b8706da30db91b4b65c250c690fb020ccbd
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-28 12:27:26 -0400

    u

--- http_scrape_pretty_local_html/http_scrape_pretty_local_html.py
+++ http_scrape_pretty_local_html/http_scrape_pretty_local_html.py
@@ -8,17 +8,19 @@ user = str(os.getlogin())
 
 if os.name == 'nt':
     # '\' works as formatter to extend to multiple lines.
-    file_url = 'file:///C:/Users/' + user + \
-        '/development/python/http_scrape_pretty_local_html/index.html'
+    work_dir = 'file:///C:\\Users\\' + user + \
+        '\\development\\python\\http_scrape_pretty_local_html\\'
+    in_url = work_dir + 'index.html'
 
 elif os.name == 'posix':
-    file_url = 'file:///home/' + user + \
-        '/development/python/http_scrape_pretty_local_html/index.html'
+    work_dir = 'file:///home/' + user + \
+        '/python/http_scrape_pretty_local_html/'
+    in_url = work_dir + 'index.html'
 else:
     print('unknown os, set os.name in script')
     exit()
 
-with urllib.request.urlopen(file_url) as response:
+with urllib.request.urlopen(in_url) as response:
     html = str(response.read())
 
     # format multiple lines for functions:
@@ -47,5 +49,9 @@ with urllib.request.urlopen(file_url) as response:
 # lstrip() strips whitespace on the left (as opposed to rstring() - right)
 # This will make the terminal print text pretty regardless
 #  of where spaces are on new lines.
-print(string.lstrip())
+#print(string.strip().rstrip().lstrip())
+
+out_file = open(work_dir + 'new.txt', 'w')
+out_file.write(string)
+out_file.close()
 exit(0)
