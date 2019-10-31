3f85e059874f835d1c4a7f178af6357810b25185
CrazyJ36 <crazy_j36@hotmail.com>
2019-10-31 01:27:03 -0400

    u

--- decorators-usage.py
+++ decorators-usage.py
@@ -3,19 +3,24 @@
 # decorators allow to reuse A function in A new way to extend it's function.
 
 # or @decor to call this below
-def decor(func):
-  def wrap():
-    print("-----")
-    func()
-    print("-----")
 
-  return wrap
+
+def decor(func):
+    def wrap():
+        print("-----")
+        func()
+        print("-----")
+
+    return wrap
+
 
 def print_text1():
-  print("Hello")
+    print("Hello")
+
 
 def print_text2():
-  print("World")
+    print("World")
+
 
 decor1 = decor(print_text1)
 decor2 = decor(print_text2)
@@ -25,10 +30,13 @@ decor2()
 print()  # skip line
 
 # The same, simpler
+
+
 def title(text):
-  print("-----")
-  print(text)
-  print("-----")
+    print("-----")
+    print(text)
+    print("-----")
+
 
 title("Hello")
 title("World")
