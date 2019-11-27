e031c447b4af5e398ae52755ddf0bbb39a5c299c
CrazyJ36 <crazy_j36@hotmail.com>
2019-11-27 02:28:39 -0500

    u

--- pysimplegui/pysimpleguiweb_test.py
+++ pysimplegui/pysimpleguiweb_test.py
@@ -1,18 +1,18 @@
 import PySimpleGUIWeb as Gui
 
 layout = [
-  [Gui.Text('text')],
-  [Gui.Button('button')]
+    [Gui.Text('text')],
+    [Gui.Button('button')]
 ]
 
 win = Gui.Window('Title', layout, web_port=8086)
 
 while True:
-  event, values = win.Read()
-  if event is None:
-    break
-  elif event == 'button':
-    print("user pressed button on web app")
+    event, values = win.Read()
+    if event is None:
+        break
+    elif event == 'button':
+        print("user pressed button on web app")
 
 win.Close()
 exit(0)
