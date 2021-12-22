78a76c18a7fe59f01f1225b646904023e7707218
CrazyJ36 <crazy_j36@hotmail.com>
2021-12-22 03:37:25 -0500

    u

--- pi/buzzer-test.py
+++ pi/buzzer-test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from gpiozero import Buzzer
-from time import sleep
 
 bz = Buzzer(21)
-bz.beep(0.005,0.005,50, False)
+bz.beep(0.01,0.01,5, False)
--- pi/led-test.py
+++ pi/led-test.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 
-print("Loading gpiozero")
-
 from gpiozero import LED
 from time import sleep
 
 led = LED(4)
 
-print("Lighting led for 1 second")
 led.on()
 sleep(1)
 led.off()
