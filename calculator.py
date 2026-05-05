e92642988f66a01240befd7b05dbd76c4eab0651
CrazyJ36 <crazy_j36@hotmail.com>
2026-05-05 08:56:11 -0400

    toying around

--- calculator.py
+++ calculator.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python3
 
-
-# define what the add funciton is
 def add(x, y):
     return x + y
 
-
-# define what the subtract funtion is
 def sub(x, y):
     return x - y
 
+def multiply(x, y):
+    return x * y
 
-# make sure 1st input is numbers only
+def divide(x, y):
+    return x / y
+
 try:
-    num1 = int(input("number1:    "))  # num1 should only be number
-except ValueError:  # if num1 is not int, state to user 'try again with number'
+    num1 = int(input("number1: "))
+except ValueError:
     print("number 1 must be 0-9")
-    exit()  # exit the program at that point
-
-# make sure character is '+' or '-' only
-sym = input("+, -:       ")
-if sym != '+' and sym != '-':  # if sym is not the + or - characters, stop
-    print("use only '+' or '-' character as equator")
     exit()
 
-# make sure 2nd input is number only
+sym = input("(+, -, *, /): ")
+if sym != '+' and sym != '-' and sym != '*' and sym != '/':
+    print("use only '+', '-', '*' or '/' character as equator")
+    exit()
+
 try:
-    num2 = int(input("number2:    "))
+    num2 = int(input("number2: "))
 except ValueError:
     print("number 2 must be 0-9")
     exit()
 
-# get input and use functions to add or subtract based on input
 if sym == "+":
-    print("answer:    ", add(num1, num2))
+    print("answer: ", add(num1, num2))
 elif sym == "-":
-    print("answer:    ", sub(num1, num2))
+    print("answer: ", sub(num1, num2))
+elif sym == "*":
+    print("answer: ", multiply(num1, num2))
+elif sym == "/":
+    print("answer: ", divide(num1, num2))
 else:
     print("Unknown Equation")
